07fc46a8dc475d8d05c3d4b216cd7e7a533d4866
Yana <44026935+yanajulias@users.noreply.github.com>
2023-06-03 03:25:13 +0700

    Make Constants.kt for constants URL

---
+++ app/src/main/java/parkee/technical/test/movieapp/util/Constants.kt
@@ -0,0 +1,15 @@
+package parkee.technical.test.movieapp.util
+
+class Constants {
+
+    companion object{
+        private const val API_KEY = "a0dd7e223fadf4e7bfa1b7eebe381267"
+        const val BEARER = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMGRkN2UyMjNmYWRmNGU3YmZhMWI3ZWViZTM4MTI2NyIsInN1YiI6IjY0NzZkMGVlMjU1ZGJhMDBjNjJmZTU2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zE-nm-QCLksJSFJOlODVavSmYKQoMY3gGGCSn1n7Cuw"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMAGE_URL = "https://image.tmdb.org/t/p/w342"
+        const val POPULAR_MOVIE_ENDPOINT = "movie/popular?api_key=$API_KEY"
+        const val TOP_RATED = "movie/top_rated?api_key=$API_KEY"
+        const val NOW_PLAYING = "movie/now_playing?api_key=$API_KEY"
+        const val MOVIE_REVIEWS = "movie/{movieId}/reviews?api_key=$API_KEY"
+    }
+}
