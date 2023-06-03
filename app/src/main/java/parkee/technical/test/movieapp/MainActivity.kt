fdc6c5a153515d8c12fc4da3767a3e89915d63ff
Yana <44026935+yanajulias@users.noreply.github.com>
2023-06-04 05:32:41 +0700

    Add logic for show list of 3 movie category in MainActivity.kt

--- app/src/main/java/parkee/technical/test/movieapp/MainActivity.kt
+++ app/src/main/java/parkee/technical/test/movieapp/MainActivity.kt
@@ -1,11 +1,115 @@
 package parkee.technical.test.movieapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import parkee.technical.test.movieapp.adapter.ListPopularMovieAdapter
+import parkee.technical.test.movieapp.adapter.ListTopRatedAdapter
+import parkee.technical.test.movieapp.data.response.MovieItem
+import parkee.technical.test.movieapp.databinding.ActivityMainBinding
+import parkee.technical.test.movieapp.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
+    private val adapter by lazy {
+        ListPopularMovieAdapter {
+            Intent(this, DetailMovieActivity::class.java).apply {
+                putExtra("movie_id", it.id)
+                startActivity(this)
+            }
+        }
+    }
+    private val adapterTopRated by lazy{
+        ListTopRatedAdapter {
+            Intent(this, DetailMovieActivity::class.java).apply {
+                putExtra("movie_id", it.id)
+                startActivity(this)
+            }
+        }
+    }
+    private val adapterNowPlaying by lazy{
+        ListTopRatedAdapter {
+            Intent(this, DetailMovieActivity::class.java).apply {
+                putExtra("movie_id", it.id)
+                startActivity(this)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel.apply {
+            getDetailPopularMovie()
+            getTopRatedMovie()
+            getNowPlayingMovie()
+        }
+
+        supportActionBar?.hide()
+
+        binding.apply {
+            rvPopularMovie.layoutManager =
+                LinearLayoutManager(applicationContext, RecyclerView.HORIZONTAL, false)
+            rvPopularMovie.setHasFixedSize(true)
+
+            rvTopRatedMovie.layoutManager =
+                LinearLayoutManager(applicationContext, RecyclerView.HORIZONTAL, false)
+            rvTopRatedMovie.setHasFixedSize(true)
+
+            rvNowPlaying.layoutManager =
+                LinearLayoutManager(applicationContext, RecyclerView.HORIZONTAL, false)
+            rvNowPlaying.setHasFixedSize(true)
+        }
+
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+
+        mainViewModel.popularMovie.observe(this) {
+            setPopularMovie(it)
+        }
+
+        mainViewModel.topRatedMovie.observe(this){
+            setTopRatedMovie(it)
+        }
+
+        mainViewModel.nowPlayingMovie.observe(this){
+            setNowPlayingMovie(it)
+        }
+
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun setPopularMovie(listPopularMovie: MutableList<MovieItem>) {
+        val adapter = adapter
+        adapter.setData(listPopularMovie)
+        binding.rvPopularMovie.adapter = adapter
+    }
+
+    private fun setTopRatedMovie(listTopRatedMovie: MutableList<MovieItem>) {
+        val adapter = adapterTopRated
+        adapter.setData(listTopRatedMovie)
+        binding.rvTopRatedMovie.adapter = adapter
+    }
+
+    private fun setNowPlayingMovie(listNowPlayingMovie: MutableList<MovieItem>) {
+        val adapter = adapterNowPlaying
+        adapter.setData(listNowPlayingMovie)
+        binding.rvNowPlaying.adapter = adapter
     }
 }
