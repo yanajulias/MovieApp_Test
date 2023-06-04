5cb0c21c159a5283dae159f8a17c5229bc884fa1
Yana <44026935+yanajulias@users.noreply.github.com>
2023-06-05 01:03:09 +0700

    Add FavoriteActivity.kt

---
+++ app/src/main/java/parkee/technical/test/movieapp/FavoriteActivity.kt
@@ -0,0 +1,18 @@
+package parkee.technical.test.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import parkee.technical.test.movieapp.databinding.ActivityFavoriteActivityBinding
+
+class FavoriteActivity : AppCompatActivity() {
+
+    private lateinit var favoriteActivityBinding: ActivityFavoriteActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        favoriteActivityBinding = ActivityFavoriteActivityBinding.inflate(layoutInflater)
+        setContentView(favoriteActivityBinding.root)
+
+
+    }
+}
