d72db2880398cd9541fea39d160ee486de918aa9
Yana <44026935+yanajulias@users.noreply.github.com>
2023-06-05 09:42:32 +0700

    Implement favorite list logic into database in DetailMovieActivity.kt and DetailMovieViewModel.kt

--- app/src/main/java/parkee/technical/test/movieapp/ui/DetailMovieActivity.kt
+++ app/src/main/java/parkee/technical/test/movieapp/ui/DetailMovieActivity.kt
@@ -1,40 +1,73 @@
 package parkee.technical.test.movieapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import androidx.activity.viewModels
+import androidx.annotation.ColorRes
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import parkee.technical.test.movieapp.R
 import parkee.technical.test.movieapp.adapter.ListReviewAdapter
 import parkee.technical.test.movieapp.data.response.MovieDetailsResponse
+import parkee.technical.test.movieapp.data.response.MovieItem
 import parkee.technical.test.movieapp.data.response.Result
 import parkee.technical.test.movieapp.databinding.ActivityDetailMovieBinding
+import parkee.technical.test.movieapp.local.DbModule
 import parkee.technical.test.movieapp.util.Constants
 import parkee.technical.test.movieapp.viewmodel.DetailMovieViewModel
 
 class DetailMovieActivity : AppCompatActivity() {
 
     private lateinit var detailMovieBinding: ActivityDetailMovieBinding
-    private val detailMovieViewModel by viewModels<DetailMovieViewModel>()
+    private val detailMovieViewModel by viewModels<DetailMovieViewModel> {
+        DetailMovieViewModel.Factory(DbModule(this))
+    }
     private val adapterReview by lazy {
         ListReviewAdapter()
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         detailMovieBinding = ActivityDetailMovieBinding.inflate(layoutInflater)
         setContentView(detailMovieBinding.root)
 
+        val movieId = intent.getParcelableExtra("movie_id", MovieItem::class.java)
+        val url = Constants.MOVIE_URL + movieId?.id
+
         detailMovieBinding.apply {
             rvListResult.layoutManager = LinearLayoutManager(applicationContext)
             rvListResult.setHasFixedSize(true)
+            btnShare.setOnClickListener {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra("Share this..", url)
+
+                val chooser = Intent.createChooser(intent, "Share using..")
+                startActivity(chooser)
+            }
+
+            btnFavorite.setOnClickListener {
+                if (movieId != null) {
+                    detailMovieViewModel.setFavoriteMovie(movieId)
+                }
+            }
         }
 
-        val movieId = intent.getIntExtra("movie_id", 0)
         detailMovieViewModel.apply {
-            getReviewMovieDetail(movieId)
-            getMovieDetail(movieId)
+            if (movieId != null) {
+                getReviewMovieDetail(movieId.id)
+            }
+            if (movieId != null) {
+                getMovieDetail(movieId.id)
+            }
         }
 
         detailMovieViewModel.movieDetail.observe(this) {
@@ -47,6 +80,20 @@ class DetailMovieActivity : AppCompatActivity() {
 
         detailMovieViewModel.reviewMovieDetail.observe(this) {
             setReviewMovie(it)
+        }
+
+        movieId?.id?.let {
+            detailMovieViewModel.findFavorite(it) {
+                detailMovieBinding.btnFavorite.changeIconColor(R.color.red)
+            }
+        }
+
+        detailMovieViewModel.favoritedMovie.observe(this) {
+            detailMovieBinding.btnFavorite.changeIconColor(R.color.red)
+        }
+
+        detailMovieViewModel.deletedFavoriteMovie.observe(this) {
+            detailMovieBinding.btnFavorite.changeIconColor(R.color.white)
         }
     }
 
@@ -75,5 +122,8 @@ class DetailMovieActivity : AppCompatActivity() {
         }
     }
 
+    private fun FloatingActionButton.changeIconColor(@ColorRes color: Int) {
+        imageTintList = ColorStateList.valueOf(ContextCompat.getColor(this.context, color))
+    }
 }
 
--- app/src/main/java/parkee/technical/test/movieapp/viewmodel/DetailMovieViewModel.kt
+++ app/src/main/java/parkee/technical/test/movieapp/viewmodel/DetailMovieViewModel.kt
@@ -4,15 +4,20 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import parkee.technical.test.movieapp.data.response.MovieDetailsResponse
+import parkee.technical.test.movieapp.data.response.MovieItem
 import parkee.technical.test.movieapp.data.response.MovieReviewsResponse
 import parkee.technical.test.movieapp.data.response.Result
 import parkee.technical.test.movieapp.data.retrofit.ApiConfig
+import parkee.technical.test.movieapp.local.DbModule
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class DetailMovieViewModel : ViewModel() {
+class DetailMovieViewModel(private val db: DbModule) : ViewModel() {
 
     private val _movieDetail = MutableLiveData<MovieDetailsResponse>()
     val movieDetail: LiveData<MovieDetailsResponse> = _movieDetail
@@ -22,6 +27,14 @@ class DetailMovieViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _favoritedMovie = MutableLiveData<Boolean>()
+    var favoritedMovie: LiveData<Boolean> = _favoritedMovie
+
+    private val _deletedFavoriteMovie = MutableLiveData<Boolean>()
+    var deletedFavoriteMovie: LiveData<Boolean> = _deletedFavoriteMovie
+
+    private var isFavorite = false
 
     fun getReviewMovieDetail(movieId: Int) {
         _isLoading.postValue(true)
@@ -69,7 +82,36 @@ class DetailMovieViewModel : ViewModel() {
         })
     }
 
+    fun setFavoriteMovie(movie: MovieItem) {
+        viewModelScope.launch {
+            movie.let {
+                if (isFavorite) {
+                    db.movieDao.delete(movie)
+                    _deletedFavoriteMovie.value = true
+                } else {
+                    db.movieDao.insert(movie)
+                    _favoritedMovie.value = true
+                }
+            }
+            isFavorite = !isFavorite
+        }
+    }
+
+    fun findFavorite(id: Int, listenFavorite: () -> Unit) {
+        viewModelScope.launch {
+            val movie = db.movieDao.findById(id)
+            if (movie != null) {
+                listenFavorite()
+                isFavorite = true
+            }
+        }
+    }
+
     companion object {
         private const val TAG = "DetailMovieActivity"
+    }
+
+    class Factory(private val db: DbModule) : ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T = DetailMovieViewModel(db) as T
     }
 }
